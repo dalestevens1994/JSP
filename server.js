e8d353cfb07e270153c8a787e1ff3f3cea4d2370
dalestevens1994 <chelseadale@hotmail.co.uk>
2016-01-14 23:21:26 +0000

    players

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var io = require('socket.io').listen(port).sockets;
 
 
 console.log('################################################');
-console.log('          Starting Server v.1.1.0 Beta          ');
+console.log('          Starting Server v.1.0 Beta            ');
 console.log('     Accepting connections on port : ' + port    );
 console.log('################################################');
 
@@ -29,8 +29,63 @@ io.on('connection', function(socket){
 			updateNicknames();
 		}
 	});
+
+	var moves = function(){
+
+	}
+
+	var game = function(){
+		game.players();
+		game.turn();
+		game.board();
+		game.pieces();
+
+		pieces = {
+		'black' : {
+					pawn1: 		{'pos' :{'x': 2, 'y':'a'}, 'alive': true}, 
+					pawn2: 		{'pos' :{'x': 2, 'y':'b'}, 'alive': true}, 
+				   	pawn3: 		{'pos' :{'x': 2, 'y':'c'}, 'alive': true},
+				   	pawn4: 		{'pos' :{'x': 2, 'y':'d'}, 'alive': true},
+				   	pawn5: 		{'pos' :{'x': 2, 'y':'e'}, 'alive': true},
+				   	pawn6: 		{'pos' :{'x': 2, 'y':'f'}, 'alive': true},
+				   	pawn7: 		{'pos' :{'x': 2, 'y':'g'}, 'alive': true},
+				   	pawn8: 		{'pos' :{'x': 2, 'y':'h'}, 'alive': true},
+				   	Castle1: 	{'pos' :{'x': 1, 'y':'a'}, 'alive': true},
+					Castle2: 	{'pos' :{'x': 1, 'y':'h'}, 'alive': true},
+				   	Knight1: 	{'pos' :{'x': 1, 'y':'b'}, 'alive': true},
+				   	Knight2: 	{'pos' :{'x': 1, 'y':'g'}, 'alive': true},
+				   	Bishop1: 	{'pos' :{'x': 1, 'y':'c'}, 'alive': true},
+				   	Bishop2: 	{'pos' :{'x': 1, 'y':'f'}, 'alive': true},
+				   	Queen: 		{'pos' :{'x': 1, 'y':'d'}, 'alive': true},
+				   	King: 		{'pos' :{'x': 1, 'y':'e'}, 'alive': true},
+				},
+		'white' : {
+					pawn1: 		{'pos' :{'x': 7, 'y':'a'}, 'alive': true}, 
+					pawn2: 		{'pos' :{'x': 7, 'y':'b'}, 'alive': true},
+				   	pawn3: 		{'pos' :{'x': 7, 'y':'c'}, 'alive': true},
+				   	pawn4: 		{'pos' :{'x': 7, 'y':'d'}, 'alive': true},
+				   	pawn5: 		{'pos' :{'x': 7, 'y':'e'}, 'alive': true},
+				   	pawn6: 		{'pos' :{'x': 7, 'y':'f'}, 'alive': true},
+				   	pawn7: 		{'pos' :{'x': 7, 'y':'g'}, 'alive': true},
+				   	pawn8: 		{'pos' :{'x': 7, 'y':'h'}, 'alive': true},
+				   	Castle1: 	{'pos' :{'x': 8, 'y':'a'}, 'alive': true},
+					Castle2: 	{'pos' :{'x': 8, 'y':'h'}, 'alive': true},
+				   	Knight1: 	{'pos' :{'x': 8, 'y':'b'}, 'alive': true},
+				   	Knight2: 	{'pos' :{'x': 8, 'y':'g'}, 'alive': true},
+				   	Bishop1: 	{'pos' :{'x': 8, 'y':'c'}, 'alive': true},
+				   	Bishop2: 	{'pos' :{'x': 8, 'y':'f'}, 'alive': true},
+				   	Queen: 		{'pos' :{'x': 8, 'y':'d'}, 'alive': true},
+				   	King: 		{'pos' :{'x': 8, 'y':'e'}, 'alive': true},
+				},
+		}
+	}
 	
 	function updateNicknames(){
+		if (nicknames.length == 0){
+			players[0] = nicknames;
+		} else if (nicknames.length == 1) {
+
+		}
 		io.emit('usernames', nicknames);
 	}
 
