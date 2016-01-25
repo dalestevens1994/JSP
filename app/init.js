7c00c8f30266d7e22d7afff9331e80c82b07ed99
dalestevens1994 <chelseadale@hotmail.co.uk>
2016-01-25 01:40:23 +0000

    All of the commits
    carrot, leak and broccoli pie. #peanutbutteroreos

--- app/init.js
+++ app/init.js
@@ -5,13 +5,35 @@ var users = '#users';
 var messageForm = '#send-message';
 var messageBox = '#message';
 var chat = '#chat';
-            
+
+var click_count = 0;
+var start_pos = null;
+var end_pos = null;
+var name = null;
+var test_data = null;
+
+
+//tempory piece storage
+var start_peice_id = null;
+var peice_img = null;
+var end_peice_id = null;
+
+ 
+function chat_message(nick, msg){
+    $(chat).append('<b>' + nick + ': </b>' + msg + "<br/>");
+}
+
+function console_message(msg){
+    $(chat).append('<span style="border: 1px solid red; padding: 5px;"><b>Console: </b>' + msg + "</span><br/>");
+}
+
 $(document).ready(function(){
-    console.log('loading app');
+    console_message('Loading game...');
 
     $('#setNick').click(function(){
-        console.log('form submited');
-        socket.emit('new_user', $(nickBox).val(), function(data){
+        console_message('Setting Nick To' + $(nickBox).val());
+        name = $(nickBox).val();
+        socket.emit('user', $(nickBox).val(), function(data){
             console.log('sending user name');
             if(data){
                 $('#nickWrap').hide();
@@ -23,10 +45,51 @@ $(document).ready(function(){
         $(nickBox).val('');
     });
 
-    $(messageForm).submit(function(e){
-        e.preventDefault();
-        socket.emit('send_message', $(messageBox).val());
-        $(messageBox).val('');
+    $('#board td').click(function(){
+        console_message('you have clicked on ' + $(this).attr('data-pos-x') + $(this).attr('data-pos-y'));
+
+        if(click_count != 2){
+
+            if(click_count == 0) {
+                peice_img = $(this).children('img').attr('src');
+                start_peice_id = $(this).attr('id');
+                start_pos = {
+                    'x': $(this).attr('data-pos-x'),
+                    'y': $(this).attr('data-pos-y')
+                }
+            }
+
+            if(click_count == 1) {
+                end_peice_id = $(this).attr('id');
+                end_pos = {
+                    'x': $(this).attr('data-pos-x'),
+                    'y': $(this).attr('data-pos-y')
+                }
+            }
+
+            $(this).css({'border': '3px solid blue'});
+            click_count++;  
+
+            if(click_count == 2){
+                //set positions from x,y to x,y piece and player.
+                move = {
+                    'from' :    {'x':start_pos.x, 'y':start_pos.y},
+                    'to' :      {'x':end_pos.x, 'y':end_pos.y},
+                    'player' :  name,
+                    'peice_img': peice_img,
+                    'start_peice_id' : start_peice_id,
+                    'end_peice_id' : end_peice_id
+
+                }
+                socket.emit('Move', move);
+                //clear move
+                start_pos = null;
+                end_pos = null;
+            }
+
+        } else {
+           
+        }
     });
 
     socket.on('usernames', function(data){
@@ -38,6 +101,32 @@ $(document).ready(function(){
     });
 
     socket.on('new_message', function(data){
-        $(chat).append('<b>' + data.nick + ': </b>' + data.msg + "<br/>");
+        chat_message(data.nick, data.msg)
+    });
+
+    socket.on('init', function(data){
+        test_data = data;
+        console.log(test_data);
+        var message = 'Welcome, Game is now starting. ' + data.player1.nick + ' is ' + data.player1.colour +  ' and ' + data.player2.nick + ' is ' + data.player2.colour + ', White player goes firdt. GLHF FIGHT!!!.';
+        console_message(message);
+    });
+
+    socket.on('move_confirmation', function(data){
+        if(data.confirm == true){
+           console.log('got move confirmation');
+            $('#board td').removeAttr('style');
+            click_count = 0;
+            console_message('move accepted'); 
+
+            console.log('start peices id = ' + data.start_peice_id);
+            console.log('img = ' + data.peice_img);
+            console.log('end peice id = ' + data.end_peice_id);
+
+            $('#' + data.start_peice_id).empty();
+            $('#' + data.end_peice_id).empty();
+            $('#' + data.end_peice_id).append('<img src="' + data.peice_img + '">');   
+        }
     });
 });
+
+
--- server.js
+++ server.js
@@ -1,109 +1,205 @@
 
-var	nicknames = [];
+//The game details object
+function game (){
+}
+game.prototype.players = [];
+game.prototype.startGame = false;
+game.prototype.turn = null;
+//object containing 2 arrays filled with JSON objects. {} = JSON object. [] = array
+game.prototype.pieces = {
+	'black' : [
+				{'piece' : 'pawn1', 'pos' :{'x': 2, 'y':'a'}, 'alive': true}, 
+				{'piece' : 'pawn2', 'pos' :{'x': 2, 'y':'b'}, 'alive': true}, 
+			   	{'piece' : 'pawn3', 'pos' :{'x': 2, 'y':'c'}, 'alive': true},
+			   	{'piece' : 'pawn4', 'pos' :{'x': 2, 'y':'d'}, 'alive': true},
+			   	{'piece' : 'pawn5', 'pos' :{'x': 2, 'y':'e'}, 'alive': true},
+			   	{'piece' : 'pawn6', 'pos' :{'x': 2, 'y':'f'}, 'alive': true},
+			   	{'piece' : 'pawn7', 'pos' :{'x': 2, 'y':'g'}, 'alive': true},
+			   	{'piece' : 'pawn8', 'pos' :{'x': 2, 'y':'h'}, 'alive': true},
+			   	{'piece' : 'Castle1','pos' :{'x': 1, 'y':'a'}, 'alive': true},
+				{'piece' : 'Castle2', 'pos' :{'x': 1, 'y':'h'}, 'alive': true},
+			   	{'piece' : 'Knight1', 'pos' :{'x': 1, 'y':'b'}, 'alive': true},
+			   	{'piece' : 'Knight2', 'pos' :{'x': 1, 'y':'g'}, 'alive': true},
+			   	{'piece' : 'Bishop1', 'pos' :{'x': 1, 'y':'c'}, 'alive': true},
+			   	{'piece' : 'Bishop2', 'pos' :{'x': 1, 'y':'f'}, 'alive': true},
+			   	{'piece' : 'Queen',	'pos' :{'x': 1, 'y':'d'}, 'alive': true},
+			   	{'piece' : 'King', 	'pos' :{'x': 1, 'y':'e'}, 'alive': true}
+			],
+	'white' : [
+				{'piece' : 'pawn1', 'pos' :{'x': 7, 'y':'a'}, 'alive': true}, 
+				{'piece' : 'pawn2', 'pos' :{'x': 7, 'y':'b'}, 'alive': true},
+			   	{'piece' : 'pawn3', 'pos' :{'x': 7, 'y':'c'}, 'alive': true},
+			   	{'piece' : 'pawn4', 'pos' :{'x': 7, 'y':'d'}, 'alive': true},
+			   	{'piece' : 'pawn5', 'pos' :{'x': 7, 'y':'e'}, 'alive': true},
+			   	{'piece' : 'pawn6', 'pos' :{'x': 7, 'y':'f'}, 'alive': true},
+			   	{'piece' : 'pawn7', 'pos' :{'x': 7, 'y':'g'}, 'alive': true},
+			   	{'piece' : 'pawn8', 'pos' :{'x': 7, 'y':'h'}, 'alive': true},
+			   	{'piece' : 'Castle1', 'pos' :{'x': 8, 'y':'a'}, 'alive': true},
+				{'piece' : 'Castle2', 'pos' :{'x': 8, 'y':'h'}, 'alive': true},
+			   	{'piece' : 'Knight1', 'pos' :{'x': 8, 'y':'b'}, 'alive': true},
+			   	{'piece' : 'Knight2', 'pos' :{'x': 8, 'y':'g'}, 'alive': true},
+			   	{'piece' : 'Bishop1', 'pos' :{'x': 8, 'y':'c'}, 'alive': true},
+			   	{'piece' :  'Bishop2', 'pos' :{'x': 8, 'y':'f'}, 'alive': true},
+			   	{'piece' : 'Queen', 'pos' :{'x': 8, 'y':'d'}, 'alive': true},
+			   	{'piece' : 'King', 'pos' :{'x': 8, 'y':'e'}, 'alive': true}
+			],
+	};
+	
+game.prototype.setNicknames = function(nickname){
+	var state = null;
+	if (this.players.length == 0){
+		this.players[0] = {'nick': nickname, 'colour': 'white'};
+		state = false;
+	} else if (this.players.length == 1) {
+		this.players[1] = {'nick': nickname, 'colour': 'black'};
+		state = true;
+	} else {
+		this.players.push({'nick':nickname, 'colour': null});
+	}
+	console.info(this.players);	
+	return state;	
+}
 
-//Start the server!!!!
+//Game rules object
+function rules(){
+}
+rules.prototype.check_move = function(){
+	console.log('checking valid move');
+	return true;
+}
+
+//Communications object
+function communications(){
+};
+communications.prototype.currentPlayers = function(players){
+	io.emit('usernames', players);
+};
+communications.prototype.gameInitialisation = function(players){
+	io.emit('init', players);
+};
+communications.prototype.turn = function(){
+	io.emit('turn', "hello");
+};
+communications.prototype.win = function(){
+	io.emit('win', "player white wins");
+};
+
+
+//Start the server!
 var port = 8080;
-//var io = require('/usr/local/lib/node_modules/socket.io').listen(port).sockets;
 var io = require('socket.io').listen(port).sockets;
-
+//var io = require('/usr/local/lib/node_modules/socket.io').listen(port).sockets;
 
 console.log('################################################');
 console.log('          Starting Server v.1.0 Beta            ');
 console.log('     Accepting connections on port : ' + port    );
 console.log('################################################');
 
-// app.get('/', function(req, res){
-// 	res.sendfile(__dirname + '/index.html');
-// });
+var current_game = new game();
+var communication = new communications();
+var check_moves = new rules();
+
+
 
 io.on('connection', function(socket){
-	console.log('new connection');
-	socket.on('new_user', function(data, callback){
+
+	socket.on('user', function(data, callback){
 		console.log('new_user');
-		if (nicknames.indexOf(data) != -1){
-			callback(false);
-		} else{
-			callback(true);
-			socket.nickname = data;
+		callback(true);
 
-			//check to make sure you get the nicknames object
-			console.log(socket.nickname);
+		//run nicknames function
+		current_game.startGame = current_game.setNicknames(data);
+		if(current_game.startGame === true){
+			var players = {'player1' : current_game.players[0], 'player2' : current_game.players[1]};
+			current_game.turn = current_game.players[0].nick;
+			communication.gameInitialisation(players);
+		}
+		console.info(data);
+	});
 
-			//run nicknames function
-			updateNicknames(socket.nickname);
+	socket.on('Move', function(data, callback){
+		console.log('check ' + data.player + ' = ' + current_game.turn);
+
+		if (data.player == current_game.turn){
+			console.log('is players turn');
+			console.log(current_game.players);
+			//check players colour
+			for (i = 0; i < current_game.players.length; ++i){
+				console.log('current loop number ' + i);
+				var p = current_game.players[i];
+				if (p.nick == data.player){
+					var player_number = i;
+					var c = p.colour;
+
+					console.log('player number = ' + player_number + 'colour = ' + c);
+				}
+			}
+
+			//loop through pieces if piece x and y = x and y and get the piece being moved
+			console.log('loop number two: ' + c);
+			console.log(current_game.pieces[c]);
+			for(i = 0; i < 16; ++i){
+				var piece = current_game.pieces[c][i];
+				console.log(piece);
+				//check alive
+				if (piece.pos.x == data.from.x && piece.pos.y == data.from.y && piece.alive == true){
+					console.log('found piece ' + piece);
+					//check valid move
+					valid = check_moves.check_move();
+					//update position
+					if(valid == true){
+						piece.position.y == data.to.y;
+						piece.position.x == data.to.x;
+
+						console.log('set update position of piece');
+					}
+
+				}
+			}
+
+			//remove any taken pieces
+			for(i = 0; i < 16; ++i){
+				console.log('found piece to take');
+				var piece = current_game.pieces[c][i];
+				if (piece.pos.x == data.to.x && piece.pos.y == data.to.y && piece.alive == true){
+					piece.alive == false;
+
+					console.log('killed piece');
+				}
+			}
+
+
+			//check for check / checkmate
+
+			//send move confirmation
+			confirmation = {
+					'confirm': true,
+				    'piece_img': data.piece_img,
+                    'start_piece_id' : data.start_piece_id,
+                    'end_piece_id' : data.end_piece_id
+			}
+
+			io.emit('move_confirmation', confirmation);
+			console.log('emit move confirmation');
+			//swap turns
+			if(player_number == 0){
+				console.log('set players turn to player 1');
+				//set player to player 1
+				current_game.turn = current_game.players[1].nick;
+			} else {
+				console.log('set players turn to player 0');
+				//set player to player 0
+				current_game.turn = current_game.players[0].nick;
+			}
+
+		} else {
+			// send a socket on the one it came in on
 		}
 	});
 
-	var moves = function(){
+	socket.on('message', function(data,callback){
+		io.emit('new_message', {msg: data, nick: 'test'});
+	});
 
-	}
-
-	var game = function(){
-		game.players();
-		game.turn();
-		game.board();
-		game.pieces();
-
-		pieces = {
-		'black' : {
-					pawn1: 		{'pos' :{'x': 2, 'y':'a'}, 'alive': true}, 
-					pawn2: 		{'pos' :{'x': 2, 'y':'b'}, 'alive': true}, 
-				   	pawn3: 		{'pos' :{'x': 2, 'y':'c'}, 'alive': true},
-				   	pawn4: 		{'pos' :{'x': 2, 'y':'d'}, 'alive': true},
-				   	pawn5: 		{'pos' :{'x': 2, 'y':'e'}, 'alive': true},
-				   	pawn6: 		{'pos' :{'x': 2, 'y':'f'}, 'alive': true},
-				   	pawn7: 		{'pos' :{'x': 2, 'y':'g'}, 'alive': true},
-				   	pawn8: 		{'pos' :{'x': 2, 'y':'h'}, 'alive': true},
-				   	Castle1: 	{'pos' :{'x': 1, 'y':'a'}, 'alive': true},
-					Castle2: 	{'pos' :{'x': 1, 'y':'h'}, 'alive': true},
-				   	Knight1: 	{'pos' :{'x': 1, 'y':'b'}, 'alive': true},
-				   	Knight2: 	{'pos' :{'x': 1, 'y':'g'}, 'alive': true},
-				   	Bishop1: 	{'pos' :{'x': 1, 'y':'c'}, 'alive': true},
-				   	Bishop2: 	{'pos' :{'x': 1, 'y':'f'}, 'alive': true},
-				   	Queen: 		{'pos' :{'x': 1, 'y':'d'}, 'alive': true},
-				   	King: 		{'pos' :{'x': 1, 'y':'e'}, 'alive': true},
-				},
-		'white' : {
-					pawn1: 		{'pos' :{'x': 7, 'y':'a'}, 'alive': true}, 
-					pawn2: 		{'pos' :{'x': 7, 'y':'b'}, 'alive': true},
-				   	pawn3: 		{'pos' :{'x': 7, 'y':'c'}, 'alive': true},
-				   	pawn4: 		{'pos' :{'x': 7, 'y':'d'}, 'alive': true},
-				   	pawn5: 		{'pos' :{'x': 7, 'y':'e'}, 'alive': true},
-				   	pawn6: 		{'pos' :{'x': 7, 'y':'f'}, 'alive': true},
-				   	pawn7: 		{'pos' :{'x': 7, 'y':'g'}, 'alive': true},
-				   	pawn8: 		{'pos' :{'x': 7, 'y':'h'}, 'alive': true},
-				   	Castle1: 	{'pos' :{'x': 8, 'y':'a'}, 'alive': true},
-					Castle2: 	{'pos' :{'x': 8, 'y':'h'}, 'alive': true},
-				   	Knight1: 	{'pos' :{'x': 8, 'y':'b'}, 'alive': true},
-				   	Knight2: 	{'pos' :{'x': 8, 'y':'g'}, 'alive': true},
-				   	Bishop1: 	{'pos' :{'x': 8, 'y':'c'}, 'alive': true},
-				   	Bishop2: 	{'pos' :{'x': 8, 'y':'f'}, 'alive': true},
-				   	Queen: 		{'pos' :{'x': 8, 'y':'d'}, 'alive': true},
-				   	King: 		{'pos' :{'x': 8, 'y':'e'}, 'alive': true},
-				},
-		}
-	}
 	
-	//pass the nickname from the client and store nickname into the nicknames array
-	function updateNicknames(nickname){
-		if (nicknames.length == 0){
-			nicknames.push(nickname);
-			//players[0] = nicknames;
-			players[0] = 'white';
-			console.info(nicknames);
-		} else if (nicknames.length == 1) {
-
-		}
-		io.emit('usernames', nicknames);
-	}
-
-	socket.on('send_message', function(data){
-		io.emit('new_message', {msg: data, nick: socket.nickname});
-	});
-	
-	socket.on('disconnect', function(data){
-		if(!socket.nickname) return;
-		nicknames.splice(nicknames.indexOf(socket.nickname), 1);
-		updateNicknames();
-	});
 });
